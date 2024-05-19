3afae664968f300bf22937f80d0c588d97465c69
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-05-19 19:26:18 +0100

    Created Track component with it's styling

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "Track.css";
+
+function Track() {
+  const renderAction = (isRemoval) => (isRemoval ? "-" : "+");
+
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        <h3>track name will go here </h3>
+        <p>track artist will go here | track album will go here </p>
+      </div>
+      <button className="Track-action"> {renderAction}</button>
+    </div>
+  );
+}
+
+export default Track;
