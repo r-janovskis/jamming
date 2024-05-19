a806c51833e19d960f8777ecccaba405b17f28ab
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-05-19 17:53:38 +0100

    Created SearchResults component and SearchResults.css

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import TrackList from '../TrackList/TrackList';
+import './SearchResults.css';
+
+function SearchResults() {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            <!-- Add a TrackList component -->
+        </div>
+    );
+}
+
+export default SearchResults;
