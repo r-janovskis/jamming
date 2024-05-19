b7059b752214c1e7da0d3b1c7f54f6e96949d3fc
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-05-19 19:18:36 +0100

    Added TrackList component with it's styling

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Track from "../Track/Track";
+import "./TrackList.css";
+
+function TrackList() {
+  return (
+    <div className="TrackList">
+      {/*<!-- You will add a map method that renders a set of Track components  -->*/}
+      <Track />
+      <Track />
+      <Track />
+    </div>
+  );
+}
+
+export default TrackList;
